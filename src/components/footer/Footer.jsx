f4c57a8a8a05cb9a0a839b864e94ebc67242418d
Md Faridul islam <mdfaridulislamovi@gmail.com>
2023-05-18 21:06:09 +0600

    blog page added

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,25 +6,22 @@ import {
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
+import logo from "../../assets/images/logo.jpg";
 import "./Footer.style.scss";
 
 const Footer = () => {
     return (
-        <footer className="bg-dark text-light">
+        <footer className="bg-dark text-light py-4">
             <Container>
                 <Row>
-                    <Col md={6} className="text-center text-md-left">
-                        <p>
-                            &copy; 2023{" "}
-                            <span className="website-name"> Toys-Planet </span>.
-                            All rights reserved.
-                        </p>
-                        <p>Designed and developed by Faridul Islam</p>
-                    </Col>
                     <Col
                         md={6}
-                        className="d-flex align-items-center justify-content-center"
+                        className="d-flex align-items-center flex-column"
                     >
+                        <div className="text-center">
+                            <p>Email: faridulccr@gmail.com</p>
+                            <p>Phone: 08996893987</p>
+                        </div>
                         <div className="social-icons">
                             <a
                                 href="https://www.facebook.com"
@@ -50,6 +47,26 @@ const Footer = () => {
                                 <FontAwesomeIcon icon={faInstagram} />
                             </a>
                         </div>
+                    </Col>
+                    <Col
+                        md={6}
+                        className="d-flex flex-column align-items-center"
+                    >
+                        <p className="d-flex align-items-center">
+                            &copy; 2023
+                            <img
+                                className="rounded-circle mx-2"
+                                src={logo}
+                                alt="logo"
+                                style={{ width: "30px", height: "30px" }}
+                            />
+                            <span className="website-name me-2">
+                                Toys-Planet
+                            </span>
+                            All rights reserved.
+                        </p>
+                        <p>Designed and developed by Faridul Islam</p>
+                        <p>Park Mor, Rangpur, Bangladesh.</p>
                     </Col>
                 </Row>
             </Container>
